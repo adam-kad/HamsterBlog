63942b2ba2c68db4483219f2a309e18edf67e0f5
adam-kad <mgm.kadiev@gmail.com>
2022-04-27 19:56:38 +0300

    Second commit

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail, name='post_detail'),
+    path('about/', views.about, name='about'),
+    path('post/', views.post),
+    path('contact/', views.contact),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -26,9 +27,20 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+def post(request):
+    return render(request, 'blog/post.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
